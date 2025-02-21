74c999fae891fe5093aef146ec5784f4945fef66
BombasticFantastik <artemilusenko2@gmail.com>
2025-02-21 16:50:22 +0700

    УРААА ВСЁ работает

--- Tricemus.py
+++ Tricemus.py
@@ -26,11 +26,29 @@ def TricemusСode(word,word_for_code):
     return "".join([code_vector[sym] for sym in word_for_code ])
 
 
-    for i in range(len(word_for_code)):
-        word_for_code[i]
+def BackTricemusСode(word,word_for_code):
+    alphabet='абвгдежзийклмнопрстуфхцчшщъыьэюя'
+    
+    new_word=''
+    new_word=[symb for symb in word if symb not in new_word]
     
 
+    
+    for symb in new_word:
+        alphabet=alphabet.replace(symb,'')
+    
+    new_word=''.join(new_word)+alphabet
 
-    return  sector0,sector1,sector2,sector3
+    sector0=new_word[0:8]
+    sector1=new_word[8:16]
+    sector2=new_word[16:24]
+    sector3=new_word[24:32]
 
-print(TricemusСode("бандероль",'чемодан'))
+    code_vector={}
+    for i in range(8):
+        code_vector[sector0[i]]=sector3[i]
+        code_vector[sector1[i]]=sector0[i]
+        code_vector[sector2[i]]=sector1[i]
+        code_vector[sector3[i]]=sector2[i]
+
+    return "".join([code_vector[sym] for sym in word_for_code ])
