152313ceb77f9bf190b767018f2342707b86e6ee
BombasticFantastik <artemilusenko2@gmail.com>
2025-02-22 12:56:52 +0700

    Добавил проверку на дурака

--- Balovstvo.py
+++ Balovstvo.py
@@ -1,3 +1,32 @@
-from Tricemus import TricemusСode
+from PyQt6.QtWidgets import QMainWindow, QApplication,QLabel, QCheckBox, QComboBox, QLineEdit,QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
+from PyQt6.QtCore import Qt
+import sys
 
-print(TricemusСode("бандероль"))
+
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = QCheckBox("hellow")
+        widget.setCheckState(Qt.CheckState.Checked)
+
+        # Включение трёх состояний: widget.setCheckState(Qt.PartiallyChecked)
+        # Или: widget.setTriState(True)
+        widget.stateChanged.connect(self.show_state)
+
+        self.setCentralWidget(widget)
+
+
+    def show_state(self, s):
+        print(s == Qt.CheckState.Checked)
+        print(s)
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
--- Tricemus.py
+++ Tricemus.py
@@ -1,21 +1,38 @@
 def TricemusСode(word,word_for_code):
+
+    word=word.lower()
+    word_for_code=word_for_code.lower()
+    #print(word,word_for_code)
+
     alphabet='абвгдежзийклмнопрстуфхцчшщъыьэюя'
+
+    #проверка на алфавит
+    for symb in word:
+        if symb not in alphabet:
+            return "Для кодирования допускаются слова состоящие только из букв русского алфавита"
+        
+    for symb in word_for_code:
+        if symb not in alphabet:
+            return "Для кодирования допускаются слова состоящие только из букв русского алфавита"
+
     
+    #деление на символы
     new_word=''
     new_word=[symb for symb in word if symb not in new_word]
     
 
-    
+    #удаление букв из алфавита
     for symb in new_word:
         alphabet=alphabet.replace(symb,'')
-    
+        
+    #создание массивов букв
     new_word=''.join(new_word)+alphabet
-
     sector0=new_word[0:8]
     sector1=new_word[8:16]
     sector2=new_word[16:24]
     sector3=new_word[24:32]
 
+    #создание  словаря 
     code_vector={}
     for i in range(8):
         code_vector[sector0[i]]=sector1[i]
@@ -26,8 +43,22 @@ def TricemusСode(word,word_for_code):
     return "".join([code_vector[sym] for sym in word_for_code ])
 
 
+
+#тоже самое но обратная функция
 def BackTricemusСode(word,word_for_code):
+    word=word.lower()
+    word_for_code=word_for_code.lower()
+
     alphabet='абвгдежзийклмнопрстуфхцчшщъыьэюя'
+
+    #проверка на алфавит
+    for symb in word:
+        if symb not in alphabet:
+            return "Для кодирования допускаются слова состоящие только из букв русского алфавита"
+        
+    for symb in word_for_code:
+        if symb not in alphabet:
+            return "Для кодирования допускаются слова состоящие только из букв русского алфавита"
     
     new_word=''
     new_word=[symb for symb in word if symb not in new_word]
